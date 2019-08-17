abdb8873c0d58bd76c1891f67f2095a41a475941
Ryan S <planetlucid@gmail.com>
2019-08-17 16:10:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,13 @@
 def create_an_empty_array
-  
+  my_array = []
 end
 
 def create_an_array
-  
+  my_array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
+  my_array = [1,2,3,4]
   
 end
 
