baeb4349b96e7f8ed19ab1ae263d4cc32d3be639
Ryan S <planetlucid@gmail.com>
2019-08-17 16:16:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = [1,2,3,4]
-  my_array << ("nala cat")
+  array = [1,2,3,4]
+  array << 5
 end
 
 def add_element_to_start_of_array(array, element)
