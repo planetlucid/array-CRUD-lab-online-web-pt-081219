5724cc9b86d0e67d3f17134d7ff1216d21686e33
Ryan S <planetlucid@gmail.com>
2019-08-17 16:20:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = [1,2,3,4]
-  my_array.unshift("nala cat")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
