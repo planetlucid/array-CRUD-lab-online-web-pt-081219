915347bc4d43cfd8cff711118b65bfb8ed5207d4
Ryan S <planetlucid@gmail.com>
2019-08-17 16:22:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,8 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  my_array = [1,2,3,4]
-  my_array.pop(-1)
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
